8073b64a5ee47bc02b8af3e3a0b42aa51401148d
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-09-17 18:38:51 +0900

    #2 - 스프링부트 프로젝트 초기화
    #1 에서 분석한 내용을 토대로
    스프링 부트 프로젝트 뼈대를 세움.
    개발 준비 완료.

---
+++ src/test/java/com/example/board_admin/BoardAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.board_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
