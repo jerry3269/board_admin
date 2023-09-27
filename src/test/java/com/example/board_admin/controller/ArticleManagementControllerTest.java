a6dea4ca5bc05fc8959c134fff7f3d3aaa5e45c0
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-09-27 17:17:30 +0900

    #11 - 비지니스 로직, 컨트롤러 테스트 작성
    게시글 목록, 단일 게시글, 게시글 삭제 기능을 만들 예정이므로
    이들의 테스트를 작성함.

--- src/test/java/com/example/board_admin/controller/ArticleManagementControllerTest.java
+++ src/test/java/com/example/board_admin/controller/ArticleManagementControllerTest.java
@@ -1,23 +1,37 @@
 package com.example.board_admin.controller;
 
 import com.example.board_admin.config.SecurityConfig;
+import com.example.board_admin.domain.constant.RoleType;
+import com.example.board_admin.dto.ArticleDto;
+import com.example.board_admin.dto.UserAccountDto;
+import com.example.board_admin.service.ArticleManagementService;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@DisplayName("View 컨트롤러 - 게시글 관리")
+@DisplayName("컨트롤러 - 게시글 관리")
 @Import(SecurityConfig.class)
 @WebMvcTest(ArticleManagementController.class)
 class ArticleManagementControllerTest {
 
     private final MockMvc mvc;
+    @MockBean private ArticleManagementService articleManagementService;
 
     public ArticleManagementControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -27,11 +41,75 @@ class ArticleManagementControllerTest {
     @Test
     void givenNothing_whenRequestingArticleManagementView_thenReturnsArticleManagementView() throws Exception {
         //given
+        given(articleManagementService.getArticles()).willReturn(List.of());
 
         //when && then
         mvc.perform(get("/management/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("management/articles"));
+                .andExpect(view().name("management/articles"))
+                .andExpect(model().attribute("articles", List.of()));
+        then(articleManagementService).should().getArticles();
+    }
+
+    @DisplayName("[Data][Get] 게시글 1개 - 정상 호출")
+    @Test
+    void givenArticleId_whenRequestingArticle_thenReturnsArticle() throws Exception {
+        //given
+        Long articleId = 1L;
+        ArticleDto articleDto = createArticleDto("title", "content");
+        given(articleManagementService.getArticle(articleId)).willReturn(articleDto);
+
+        //when && then
+        mvc.perform(get("/management/articles/" + articleId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(articleId))
+                .andExpect(jsonPath("$.title").value(articleDto.title()))
+                .andExpect(jsonPath("$.content").value(articleDto.content()))
+                .andExpect(jsonPath("$.userAccount.nickname").value(articleDto.userAccount().nickname()));
+        then(articleManagementService).should().getArticles();
+    }
+
+    @DisplayName("[View][Post] 게시글 삭제 - 정상 호출")
+    @Test
+    void givenArticleId_whenRequestingDeletion_thenRedirectsToArticleManagementView() throws Exception {
+        //given
+        Long articleId = 1L;
+        willDoNothing().given(articleManagementService).deleteArticle(articleId);
+
+        //when && then
+        mvc.perform(post("/management/articles/" + articleId)
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/management/articles"))
+                .andExpect(redirectedUrl("/management/articles"));
+        then(articleManagementService).should().deleteArticle(articleId);
+    }
+
+    private ArticleDto createArticleDto(String title, String content) {
+        return ArticleDto.of(
+                1L,
+                createUserAccountDto(),
+                title,
+                content,
+                null,
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "testId",
+                "testPw",
+                Set.of(RoleType.ADMIN),
+                "test@email",
+                "testNickname",
+                "testMemo"
+        );
     }
 }
