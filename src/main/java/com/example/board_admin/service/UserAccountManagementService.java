d20d214b385f699a32421df9a47fc566bd6bebb9
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-09-30 17:46:54 +0900

    #13 - 회원 관리 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ src/main/java/com/example/board_admin/service/UserAccountManagementService.java
@@ -0,0 +1,24 @@
+package com.example.board_admin.service;
+
+import com.example.board_admin.dto.UserAccountDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserAccountManagementService {
+
+    public List<UserAccountDto> getUserAccounts() {
+        return List.of();
+    }
+
+    public UserAccountDto getUserAccount(String userId) {
+        return null;
+    }
+
+    public void deleteUserAccount(String userId) {
+
+    }
+}
